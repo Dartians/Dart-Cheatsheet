26b7da89c7513532aff08839d3617acd1a16b468
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-15 21:52:36 +0330

    Add condiation

---
+++ 11-operators-5.dart
@@ -0,0 +1,10 @@
+// Operators | Conditionals
+
+void main() {
+  print(5 == 5); // Equal
+  print(5 != 5); // Not equal
+  print(5 >= 5); // Greater or equal
+  print(5 > 5); // Greater than
+  print(5 <= 5); // Less or equal
+  print(5 < 5); // Less than
+}
