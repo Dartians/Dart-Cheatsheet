2904f4fb3ecbf534964ad09532d5d62908247263
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-18 00:07:22 +0330

    Add functions - Part 3

---
+++ 41-functions-3.dart
@@ -0,0 +1,15 @@
+// Functions | Basic 3
+// Return
+
+void main() {
+  // Add this return function to a variable
+  final say = hi("Mehran");
+  // Print the variable
+  print(say);
+}
+
+// Create a function that returns string
+String hi(String name) {
+  // Using return
+  return "Hi dear $name!";
+}
