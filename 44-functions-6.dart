398e566366d7c4a3bc04165f93d69f6ed7152a84
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-18 00:07:49 +0330

    Add functions - Part 6

---
+++ 44-functions-6.dart
@@ -0,0 +1,15 @@
+// Functions | Basic 6
+// Return | =>
+
+void main() {
+  // Hi function
+  print(hi('Mehran', 1234));
+
+  // Using done
+  print(done(5, 10));
+}
+
+// If your function returns a values, use ( => )
+String hi(String name, int id) => "Hi dear $name! ID: $id";
+
+int done(int x, int y) => x + y;
