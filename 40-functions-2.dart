918f073a7a4acb3507b6ca89f4b9fc620bba9b9f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-18 00:06:16 +0330

    Add functions - Part 2

---
+++ 40-functions-2.dart
@@ -0,0 +1,17 @@
+// Functions | Basic 2
+// Arguments | 1
+
+void main() {
+  // Call function with variable
+  final name = "Amir";
+  hi(name);
+
+  // Call function with not variable
+  hi("Anahita");
+}
+
+// Create a function
+void hi(String name) {
+  // Add variables
+  print('Hi dear $name!');
+}
