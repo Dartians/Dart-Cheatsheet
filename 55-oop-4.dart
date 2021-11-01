70905749068586f4c5c4836dea144ea2d5e69786
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-01 12:15:52 +0330

    abstract

---
+++ 55-oop-4.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+abstract class Shape {
+  double get area;
+}
+
+class Square extends Shape {
+  Square(this.side);
+  final double side;
+
+  @override
+  double get area => side * side;
+}
+
+class Circle extends Shape {
+  Circle(this.radius);
+  final double radius;
+
+  @override
+  double get area => pi * radius * radius;
+}
+
+void printArea(Shape shape) {
+  print(shape.area);
+}
+
+void main() {
+  final all = [
+    Square(10),
+    Circle(5),
+  ];
+
+  all.forEach(printArea);
+}
