840444381a1eaf14d175157cda6499f458b1d643
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-01 08:47:51 +0330

    Ading new sesion

---
+++ 46-class-2.dart
@@ -0,0 +1,19 @@
+// Class | Season 2
+
+// Initialize with pars
+class BankAccount {
+  BankAccount({required double balance, required String owner})
+      : this.balance = balance,
+        this.owner = owner;
+
+  double balance;
+  String owner;
+}
+
+void main() {
+  // Init a class
+  final account = BankAccount(balance: 100, owner: "Amirhossein");
+
+  // Print owner
+  print(account.owner);
+}
