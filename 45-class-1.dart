a8df93295240e73cb53cb7114446f8dc618aa541
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-01 08:41:29 +0330

    Add first class session

---
+++ 45-class-1.dart
@@ -0,0 +1,14 @@
+// Class | Season 1
+
+// Create a class
+class BankAccount {
+  double balence = 0;
+}
+
+void main() {
+  // Add class to a var
+  final account = BankAccount();
+
+  // Print a variable
+  print(account.balence);
+}
